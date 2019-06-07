0d7dc95b4fcadf2f9e24ccebf6d3bca54c7c24cc
Alison Golden <alisong779@yahoo.com>
2019-06-07 02:43:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,19 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do 
+    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    # redirect "/recipes/#{recipe.id}"
+  end 
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params["id"])
+    erb :show
+  end 
+
 
 end
