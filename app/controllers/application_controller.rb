cb2bae56451e3d2c7b89f9089d4e90d6222dea6e
Alison Golden <alisong779@yahoo.com>
2019-06-07 02:53:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,14 @@ class ApplicationController < Sinatra::Base
     erb :show
   end 
 
-  get '/recipes/:id/edit' do 
-    @recipe = Recipe.find(params["id"])
-    erb :edit 
-  end 
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
 
 
 end
